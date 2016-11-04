2b6986b1190cf91bc64968f87804dba58f785c51
Dan Stutzman <dan@danstutzman.com>
2016-11-04 11:09:24 -0600

    Not testing CloudfrontLogs locally anymore

--- bin/run.sh
+++ bin/run.sh
@@ -2,13 +2,6 @@
 cd `dirname $0`/..
 
 go run main.go '{"PortNum": 3000,
-  "CloudfrontLogs": {
-    "S3CredsPath": "conf/s3.creds.ini",
-    "S3Region": "us-east-1",
-    "S3BucketName": "cloudfront-logs-danstutzman",
-    "GcloudPemPath": "conf/Speech-ba6281533dc8.json",
-    "GcloudProjectId": "speech-danstutzman"
-  },
   "MemoryUsage": true,
   "PiwikExporter": false,
   "SecurityUpdates": false,
